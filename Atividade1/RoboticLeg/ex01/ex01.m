6c6344f757c9995e68c4ea31cb1c5703d1919377
Fabio Grassiotto <fabiogr@gmail.com>
2018-08-28 21:25:38 -0300

    Ex01 working ok

--- Atividade1/RoboticLeg/ex01/ex01.m
+++ Atividade1/RoboticLeg/ex01/ex01.m
@@ -3,9 +3,9 @@ clc; clear all;
 syms alpha beta gamma real
 
 % write down the rotation matrices using the symbolic parameters alpha, beta, gamma
-R_B1 = ...;
-R_12 = ...;
-R_23 = ...;
+R_B1 = [ 1 0 0; 0 cos(alpha) -sin(alpha); 0 sin(alpha) cos(alpha)]; % x axis
+R_12 = [ cos(beta) 0 sin(beta); 0 1 0; -sin(beta) 0 cos(beta)]; % y axis
+R_23 = [ cos(gamma) 0 sin(gamma); 0 1 0; -sin(gamma) 0 cos(gamma)]; % y axis again
 
 % answers
 valid
