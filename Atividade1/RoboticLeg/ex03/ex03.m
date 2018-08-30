234486808403038e276a1d5a076998f7aa9f8f11
Fabio Grassiotto <fabiogr@gmail.com>
2018-08-30 08:51:47 -0300

    ex03 result ok

--- Atividade1/RoboticLeg/ex03/ex03.m
+++ Atividade1/RoboticLeg/ex03/ex03.m
@@ -11,17 +11,24 @@ r_BF_inB = [...
 
 
 % determine the foot point Jacobian J_BF_inB=d(r_BF_inB)/dq
-J_BF_inB = ...
+f = r_BF_inB;
+fx = f(1);
+fy = f(2);
+fz = f(3);
+J_BF_inB = [ diff(fx, alpha) diff(fx, beta) diff(fx, gamma); ...
+             diff(fy, alpha) diff(fy, beta) diff(fy, gamma); ...
+             diff(fz, alpha) diff(fz, beta) diff(fz, gamma) ];
+             
 
-% what generalized velocity do do you have to apply in a configuration q = [0;60°;-120°]
+% what generalized velocity dq do you have to apply in a configuration q = [0;60Â°;-120Â°]
 % to lift the foot in vertical direction with v = [0;0;-1m/s];
 v = [0; 0; -1]
 qi = [0; 60*(pi/180); -120*(pi/180)];
 
 % Determine the numerical value of the foot point jacobian for initial joint angles qi
-JBF = ...
+JBF = double(eval("subs(J_BF_inB, q, qi)"));
     
 % Determine the numerical value for dq
-dq = ...
+dq = inv(JBF)*v;
 
 valid
